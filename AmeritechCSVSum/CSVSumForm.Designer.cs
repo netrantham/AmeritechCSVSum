fc577fcada8fde1f6645f6d65607ef7336800540
nktte <nktte@DESKTOP-VPP95LE>
2024-02-05 10:22:18 -0800

    Add project files.

---
+++ AmeritechCSVSum/CSVSumForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AmeritechCSVSum
+{
+    partial class CSVSumForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.sumLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.browseButton.Location = new System.Drawing.Point(110, 192);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(125, 50);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(272, 192);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(450, 50);
+            this.outputBox.TabIndex = 1;
+            this.outputBox.Text = "0";
+            // 
+            // sumLabel
+            // 
+            this.sumLabel.AutoSize = true;
+            this.sumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.sumLabel.Location = new System.Drawing.Point(268, 165);
+            this.sumLabel.Name = "sumLabel";
+            this.sumLabel.Size = new System.Drawing.Size(54, 24);
+            this.sumLabel.TabIndex = 2;
+            this.sumLabel.Text = "Sum:";
+            // 
+            // CSVSumForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sumLabel);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.browseButton);
+            this.Name = "CSVSumForm";
+            this.Text = "CSV Sum Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label sumLabel;
+    }
+}
+
---
+++ AmeritechCSVSum/CSVSumForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmeritechCSVSum
+{
+    public partial class CSVSumForm : Form
+    {
+        public CSVSumForm()
+        {
+            InitializeComponent();
+        }
+
+        //Opens a file browser on button click, then sends the FileName to readCSV
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileBrowser = new OpenFileDialog();
+            fileBrowser.Filter = "CSV File (*.csv)|*.csv";
+
+            if (fileBrowser.ShowDialog() == DialogResult.OK)
+            {
+                //A double check on the selected file type
+                if (Path.GetExtension(fileBrowser.SafeFileName) == ".csv")
+                {
+                    readCSV(fileBrowser.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect file type of: "+ Path.GetExtension(fileBrowser.SafeFileName));
+                }
+            }
+            fileBrowser.Dispose();
+        }
+
+        //Referenced in browseButton_Click. Opens the csv with StreamReader. Reading one line and sending it to parseLine, then updateOutputBox
+        private void readCSV(string filePath)
+        {
+            //lineCounter is for an output error message in parseLine
+            int lineCounter = 1;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        string currLine = sr.ReadLine();
+                        //Removes anything not in the first column
+                        currLine = currLine.Split(',')[0];
+                        ulong lineLong = parseLine(currLine, lineCounter);
+                        updateOutputBox(lineLong);
+                        lineCounter++;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Process failed: " + e.ToString());
+            }
+        }
+
+        //Referenced in readCSV. Takes in the current line of the csv and removes non-digits then all but the last 10 digits. Then converts to ulong and returns.
+        private ulong parseLine(string line, int lineCounter)
+        {
+            //Remove non-digit characters from line
+            string currLine = Regex.Replace(line, "[^0-9]", "");
+            //Remove all but last 10 characters from string, unless string is less than 10 characters
+            currLine = currLine.Substring(Math.Max(0, currLine.Length - 10));
+
+            ulong currLong;
+            bool success = UInt64.TryParse(currLine, out currLong);
+            if (success)
+            {
+                return currLong;
+            }
+            else
+            {
+                MessageBox.Show("Conversion of string to long failed. Line number "+ lineCounter +" from file.");
+                return 0;
+            }
+        }
+
+        //Referenced in readCSV. Converts the TextBox value to ulong then adds the ulong from the current line of the csv. Converts the sum to string and updates the TextBox
+        private void updateOutputBox(ulong currLong)
+        {
+            ulong sum = 0;
+            //Converts text in outputBox to long so it can be added with the current line from the csv
+            if (!UInt64.TryParse(outputBox.Text, out sum))
+            {
+                MessageBox.Show("Conversion of string to long failed. outputBox.");
+            }
+            sum += currLong;
+            outputBox.Text = sum.ToString();
+        }
+    }
+}
